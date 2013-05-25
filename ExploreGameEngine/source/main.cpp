121f8b75aaa2a19c4a30e19220a5fd905ec36c66
Pedro Custodio <magneticplum@live.co.uk>
2013-05-25 17:20:21 +0100

    switched from Horde3D to Irrlicht

--- ExploreGameEngine/source/main.cpp
+++ ExploreGameEngine/source/main.cpp
@@ -1,16 +1,59 @@
- #include"glfw.h"
- #include"Horde3D.h"
- #include"Horde3DUtils.h"
- int main()
- {
-   glfwInit();
-   if(!glfwOpenWindow(640,480,8,8,8,8,24,8,GLFW_WINDOW))
-   {
-       glfwTerminate();
-       return 1;
-   }
-   h3dInit();
-   h3dRelease();
-   glfwCloseWindow();
-   glfwTerminate();
- }
+#include <irrlicht.h>
+
+using namespace irr;
+
+using namespace core;
+using namespace scene;
+using namespace video;
+using namespace io;
+using namespace gui;
+
+#ifdef _IRR_WINDOWS_
+#pragma comment(linker, "/subsystem:windows /ENTRY:mainCRTStartup")
+#endif
+
+
+
+
+int main()
+{
+	IrrlichtDevice* device=createDevice(video::EDT_SOFTWARE, dimension2d<u32>(640,480), 16, false, false, false, 0);
+
+	if(!device)
+		return 1;
+
+	device->setWindowCaption(L"Hello World!");
+
+	IVideoDriver* driver = device->getVideoDriver();
+	ISceneManager* smgr = device->getSceneManager();
+	IGUIEnvironment* guienv = device->getGUIEnvironment();
+
+
+
+	guienv->addStaticText(L"LOL IM DOING STUFF!!1!", rect<s32>(10,10,260,22), true);
+
+
+
+	smgr->addCameraSceneNode(0, vector3df(0,30,-40), vector3df(0,5,0));
+
+
+
+
+	while(device->run())
+	{
+		driver->beginScene(true, true, SColor(255,100,101,140));
+
+		guienv->drawAll();
+
+		driver->endScene();
+	}
+
+
+
+	device->drop();
+
+
+	return 0;
+
+
+}
